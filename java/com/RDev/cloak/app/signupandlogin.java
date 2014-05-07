cdd54c12a10a36a6f69a4e4e05780378ea200b52
rossepsteini <footballsteelershowie@gmail.com>
2014-05-07 15:02:07 -0400

    Cloak Pre-Aplha Version 0.1
    Fixes Force Closed and allows app to choose to close or start listeners
    to save battery.

---
+++ java/com/RDev/cloak/app/signupandlogin.java
@@ -0,0 +1,25 @@
+package com.RDev.cloak.app;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+
+
+public class signupandlogin extends ActionBarActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.configure);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                setContentView(R.layout.signup);
+            }
+        });
+    }
+
+
+    }
+
