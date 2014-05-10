492563d783c657e10986223497e40a2c39679f82
rossepsteini <footballsteelershowie@gmail.com>
2014-05-09 22:43:43 -0400

    Adds after login dashboard

---
+++ java/com/RDev/cloak/app/dashboard.java
@@ -0,0 +1,15 @@
+package com.RDev.cloak.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class dashboard extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nextstep);
+
+
+    }
+}
