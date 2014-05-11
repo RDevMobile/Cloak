7299a72d1e5f5d4055cbd1e5c9f1676c25aa6a11
rossepsteini <footballsteelershowie@gmail.com>
2014-05-11 18:44:25 -0400

    Adds first feature !!!!!

--- java/com/RDev/cloak/app/dashboard.java
+++ java/com/RDev/cloak/app/dashboard.java
@@ -1,8 +1,10 @@
 package com.RDev.cloak.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.CheckBox;
 
 
 public class dashboard extends Activity {
@@ -12,4 +14,22 @@ public class dashboard extends Activity {
 
 
     }
+
+public void more_info_txt(View view){
+    setContentView(R.layout.info_txt);
+}
+
+    public void go_back_txt(View view){
+        setContentView(R.layout.dashboard);
+    }
+
+    public void itemClicked(View v) {
+        //code to check if this checkbox is checked!
+        CheckBox checkBox = (CheckBox)v;
+        if(checkBox.isChecked()){
+            Intent intent2 = new Intent(this, smslistener.class);
+            startActivity(intent2);
+        }
+    }
+
 }
---
+++ java/com/RDev/cloak/app/smslistener.java
@@ -0,0 +1,77 @@
+package com.RDev.cloak.app;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+public class smslistener extends BroadcastReceiver {
+
+    private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
+    private static final String TAG = "SMSBroadcastReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Intent recieved: " + intent.getAction());
+
+        if (intent.getAction().equals(SMS_RECEIVED)) {
+            Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+                Object[] pdus = (Object[]) bundle.get("pdus");
+                final SmsMessage[] messages = new SmsMessage[pdus.length];
+                for (int i = 0; i < pdus.length; i++) {
+                    messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                }
+                if (messages.length > -1) {
+                    Toast.makeText(context, "Message recieved: " + messages[0].getMessageBody(), 7000).show();
+                }
+                String word1 = "beer";
+                String word2 = "Beer";
+                String word3 = "Pot";
+                String word4 = "pot";
+                String message = messages[0].getMessageBody();
+                if (message == word1) {
+                    CharSequence text = "Message contained the word Beer and was hidden. Go to settings to change this feature.";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+                if (message == word2) {
+                    CharSequence text = "Message contained the word Beer and was hidden. Go to settings to change this feature.";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+                if (message == word3) {
+                    CharSequence text = "Message contained the word Beer and was hidden. Go to settings to change this feature.";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+                if (message == word3) {
+                    CharSequence text = "Message contained the word Beer and was hidden. Go to settings to change this feature.";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
