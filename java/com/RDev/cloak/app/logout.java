f85938b101f224eff3d5e81df24e6e95907c793e
rossepsteini <footballsteelershowie@gmail.com>
2014-05-12 18:39:04 -0400

    Adds Auto Login

---
+++ java/com/RDev/cloak/app/logout.java
@@ -0,0 +1,19 @@
+package com.RDev.cloak.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.parse.ParseUser;
+
+public class logout extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActionBar().show();
+        ParseUser.logOut();
+        Intent intent = new Intent(this, login.class);
+        startActivity(intent);
+
+    }
+}
